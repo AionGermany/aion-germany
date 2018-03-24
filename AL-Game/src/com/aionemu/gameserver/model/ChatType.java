302391011b2307762bcfe1a1c2f0066737db4604
Michel Gorter <michelgorter@outlook.com>
2018-03-24 18:35:03 +0100

    Chattype Colors Update
    Colors are fixed wasnt yellow and not in center!

--- AL-Game/src/com/aionemu/gameserver/model/ChatType.java
+++ AL-Game/src/com/aionemu/gameserver/model/ChatType.java
@@ -48,13 +48,14 @@ public enum ChatType {
 	 * Global chat types
 	 */
 	UNION_WAR(0x19, true), // Union War message (Blue)
-
-	WHITE(0x1E, true), // System message (White), visible in "All" chat thumbnail only !
-	YELLOW(0x1F, true), // System message (Yellow), visible in "All" chat thumbnail only !
-	BRIGHT_YELLOW(0x20, true), // System message (Light Yellow), visible in "All" chat thumbnail only !
-	WHITE_CENTER(0x21, true), // Periodic Notice (White && Box on screen center)
-	YELLOW_CENTER(0x22, true), // Periodic Announcement(Yellow && Box on screen center)
-	BRIGHT_YELLOW_CENTER(0x23, true); // System Notice (Light Yellow && Box on screen center)
+	WHITE(0x23, true), // System message (White), visible in "All" chat thumbnail only !
+	YELLOW(0x24, true), // System message (Yellow), visible in "All" chat thumbnail only !
+	BRIGHT_YELLOW(0x25, true), // System message (Light Yellow), visible in "All" chat thumbnail only !
+	WHITE_CENTER(0x26, true), // Periodic Notice (White && Box on screen center)
+	YELLOW_CENTER(0x27, true), // Periodic Announcement(Yellow && Box on screen center)
+	BRIGHT_YELLOW_CENTER(0x28, true), // System Notice (Light Yellow && Box on screen center)
+	BRIGHT_YELLOW_CENTER_NEW(0x29, true), // Bright Yellow Center New
+	GMRESPONSE(0x1C); // GM Response
 
 	private final int intValue;
 	private boolean sysMsg;
