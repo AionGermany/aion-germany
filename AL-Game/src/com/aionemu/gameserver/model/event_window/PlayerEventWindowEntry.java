06b6e61d15790f2566bf065d8d6d7ca3352caa3c
Michel Gorter <michelgorter@outlook.com>
2018-06-13 23:28:52 +0200

    Update PlayerEventWindowEntry
    Cleaning Update 3

--- AL-Game/src/com/aionemu/gameserver/model/event_window/PlayerEventWindowEntry.java
+++ AL-Game/src/com/aionemu/gameserver/model/event_window/PlayerEventWindowEntry.java
@@ -1,5 +1,18 @@
-/*
- * Decompiled with CFR 0_123.
+/**
+ * This file is part of Aion-Lightning <aion-lightning.org>.
+ *
+ *  Aion-Lightning is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Aion-Lightning is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details. *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Aion-Lightning.
+ *  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aionemu.gameserver.model.event_window;
 
@@ -7,12 +20,15 @@ import java.sql.Timestamp;
 
 import com.aionemu.gameserver.model.gameobjects.PersistentState;
 
+/**
+ * @author Ghostfur (Aion-Unique)
+ */
 public class PlayerEventWindowEntry
 extends EventWindowEntry {
     private PersistentState persistentState;
 
-    public PlayerEventWindowEntry(int n2, Timestamp timestamp, int n3, PersistentState persistentState) {
-        super(n2, timestamp, n3);
+    public PlayerEventWindowEntry(int time, Timestamp timestamp, int remaining, PersistentState persistentState) {
+        super(time, timestamp, remaining);
         this.persistentState = persistentState;
     }
 
@@ -43,6 +59,5 @@ extends EventWindowEntry {
             }
         }
     }
-
 }
 
