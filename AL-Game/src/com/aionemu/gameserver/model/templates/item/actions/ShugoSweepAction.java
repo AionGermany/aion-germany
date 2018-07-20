0ead84860d0ee5d5ee755e8cc2239d3d0c679584
Falke_34 <falke.34@gmx.de>
2018-07-20 20:24:16 +0200

    small cleanup for ShugoSweepAction

--- AL-Game/src/com/aionemu/gameserver/model/templates/item/actions/ShugoSweepAction.java
+++ AL-Game/src/com/aionemu/gameserver/model/templates/item/actions/ShugoSweepAction.java
@@ -33,44 +33,46 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "ShugoSweepAction")
-public class ShugoSweepAction extends AbstractItemAction
-{
-    @XmlAttribute(name = "type") //1 reset ; 2 gold dice
-    protected int type;
-	
-    @XmlAttribute(name = "count")
-    protected boolean count;
-	
-    @Override
-    public boolean canAct(Player player, Item parentItem, Item targetItem) {
-        if (type == 1) {
-            if (getCommonData(player).getResetBoard() != 0) {
-            	PacketSendUtility.sendMessage(player, "You have already one Reset Board");
-                return false;
-            }
-        }
-        return true;
-    }
-	
-    @Override
-    public void act(Player player, Item parentItem, Item targetItem) {
-        if (player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1)) {
-            if (type == 1) {
-                getCommonData(player).setResetBoard(getCommonData(player).getResetBoard() + 1);
-                PacketSendUtility.sendMessage(player, "You have received one Reset Board");
-            } if (type == 2) {
-                getCommonData(player).setGoldenDice(getCommonData(player).getGoldenDice() + 1);
-                PacketSendUtility.sendMessage(player, "You have received one Golden Dice");
-            }
-            PacketSendUtility.sendPacket(player, new SM_SHUGO_SWEEP(getPlayerSweep(player).getBoardId(), getPlayerSweep(player).getStep(), getPlayerSweep(player).getFreeDice(), getCommonData(player).getGoldenDice(), getCommonData(player).getResetBoard(), 0));
-        }
-    }
-	
-    public PlayerCommonData getCommonData(Player player) {
-        return player.getCommonData();
-    }
-	
-    public PlayerSweep getPlayerSweep(Player player) {
-        return player.getPlayerShugoSweep();
-    }
+public class ShugoSweepAction extends AbstractItemAction {
+
+	@XmlAttribute(name = "type")
+	// 1 reset ; 2 gold dice
+	protected int type;
+
+	@XmlAttribute(name = "count")
+	protected boolean count;
+
+	@Override
+	public boolean canAct(Player player, Item parentItem, Item targetItem) {
+		if (type == 1) {
+			if (getCommonData(player).getResetBoard() != 0) {
+				PacketSendUtility.sendMessage(player, "You have already one Reset Board");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void act(Player player, Item parentItem, Item targetItem) {
+		if (player.getInventory().decreaseByObjectId(parentItem.getObjectId(), 1)) {
+			if (type == 1) {
+				getCommonData(player).setResetBoard(getCommonData(player).getResetBoard() + 1);
+				PacketSendUtility.sendMessage(player, "You have received one Reset Board");
+			}
+			if (type == 2) {
+				getCommonData(player).setGoldenDice(getCommonData(player).getGoldenDice() + 1);
+				PacketSendUtility.sendMessage(player, "You have received one Golden Dice");
+			}
+			PacketSendUtility.sendPacket(player, new SM_SHUGO_SWEEP(getPlayerSweep(player).getBoardId(), getPlayerSweep(player).getStep(), getPlayerSweep(player).getFreeDice(), getCommonData(player).getGoldenDice(), getCommonData(player).getResetBoard(), 0));
+		}
+	}
+
+	public PlayerCommonData getCommonData(Player player) {
+		return player.getCommonData();
+	}
+
+	public PlayerSweep getPlayerSweep(Player player) {
+		return player.getPlayerShugoSweep();
+	}
 }
