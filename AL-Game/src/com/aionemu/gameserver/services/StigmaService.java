7c49677445c793aaea575aab10820082688502b4
FrozenKiller <35238298+FrozenKiller35@users.noreply.github.com>
2019-04-27 01:04:24 +0200

    Added both new StigmaSlots

--- AL-Game/src/com/aionemu/gameserver/services/StigmaService.java
+++ AL-Game/src/com/aionemu/gameserver/services/StigmaService.java
@@ -481,15 +481,31 @@ public class StigmaService {
 						return true;
 				}
 				else if (stigmaCount == 4) {
-					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask())
+					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask()
+							|| itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask())
 						return true;
 				}
 				else if (stigmaCount == 5) {
-					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA5.getSlotIdMask())
+					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask()
+							|| itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA5.getSlotIdMask())
 						return true;
 				}
 				else if (stigmaCount == 6) {
+					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask()
+							|| itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA5.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA6.getSlotIdMask())
 					return true;
+				}
+				else if (stigmaCount == 7) {
+					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask()
+						|| itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA5.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA6.getSlotIdMask()
+						|| itemSlotToEquip == ItemSlot.STIGMA7.getSlotIdMask())
+						return true;
+				}
+				else if (stigmaCount == 8) {
+					if (itemSlotToEquip == ItemSlot.STIGMA1.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA2.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA3.getSlotIdMask()
+						|| itemSlotToEquip == ItemSlot.STIGMA4.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA5.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA6.getSlotIdMask()
+						|| itemSlotToEquip == ItemSlot.STIGMA7.getSlotIdMask() || itemSlotToEquip == ItemSlot.STIGMA8.getSlotIdMask())
+						return true;
 				}
 			}
 		}
